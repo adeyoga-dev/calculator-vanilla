a684fc086455d5e8c947134ee3b405280515bc9f
Muhammad Rizki Adeyoga <adeyogamuhammadrizki@gmail.com>
2024-04-12 16:20:52 +0700

    feat: support float number, print result on content, auto size depending total character on content, operator cannnot be typed the first time

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,39 @@
 let value = "";
+let first_character = 0;
 // inislalisasi element
 let text_input = document.getElementById("text-input");
 let text_result = document.getElementById("text-result");
 
-
 function getValue(element){
+    let length = value.length;
+    if(!isNumber(element.value) && value==="" || element.value==="." && value==="") return false;
+    if(length>=48) return false; //jika lebih dari sama dengan 48 karakter jangan simpan value dan print screen
     value += element.value;
     printValue(value);
 }
 
 function printValue(value){
+    let length = value.length;
+    if(length>=6 && length<24){ //jika lebih dari sama dengan 6 karakter kurangi ukuran font
+        text_input.style.fontSize = "30px";
+    }else if(length>=24){ //jika lebih dari sama dengan 24 karakter kurangi ukuran font
+        text_input.style.fontSize = "15px";
+    }else{
+        text_input.style.fontSize = "60px";
+    }
     text_input.innerHTML = value;
+}
+
+function printResult(value){
+    let length = value.toString().length;
+    if(length>=6 && length<24){ //jika lebih dari sama dengan 6 karakter kurangi ukuran font
+        text_result.style.fontSize = "30px";
+    }else if(length>=24){ //jika lebih dari sama dengan 24 karakter kurangi ukuran font
+        text_result.style.fontSize = "15px";
+    }else{
+        text_result.style.fontSize = "60px";
+    }
+    text_result.innerHTML = value;
 }
 
 function beginCount(){
@@ -35,10 +58,10 @@ function beginCount(){
 function arithmeticOperation(value){
     let length = value.length;
     let total = 0;
-    let number1 = parseInt(value[0]);
+    let number1 = parseFloat(value[0]);
     
     for(i=0; i<length; i++){
-        let number2 = parseInt(value[i+2]);
+        let number2 = parseFloat(value[i+2]);
         let result = 0;
         if(typeof value[i+1] === "undefined"){
             break;
@@ -56,16 +79,19 @@ function arithmeticOperation(value){
             case "÷":
                 result = number1 / number2;
                 break;
+            case "%":
+                result = number1 % number2;
+                break;
         }
         number1 = result;
         total = number1;
         i++
     }
-    console.log(total);
+    printResult(total);
 }
 
 function isNumber(number){
-    let number_list = "0123456789";
+    let number_list = ".0123456789";
     let length = number_list.length;
     for(let i=0; i<length; i++){
         if(number == number_list.charAt(i)) return true;
